1adebd6fdf2ccedcaea424657c719207cf49a796
WILLIAM <williamlkengne@gmail.com>
2021-09-21 14:43:46 +0000

    [ADD] ORMGOOD2 MODULE

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -4,34 +4,38 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
-    _name = 'academy.course'
-    _description = 'Course.Info'
     
-    name = fields.Char(string='Title', required = True)
+    _name = 'academy.course'
+    _description = 'Course Info'
+    
+    name = fields.Char(string = 'Title', required = True)
     description = fields.Text(string='Description')
     
     level = fields.Selection(string='Level',
                             selection=[('beginner', 'Beginner'),
                                        ('intermediate', 'Intermediate'),
                                        ('advanced', 'Advanced')],
-                            copy=False)
-    active = fields.Boolean(string='Active',default=True)
+                             copy=False)
     
-    base_price = fields.Float(string='Active', default=True)
+    active = fields.Boolean(string='Active', default = True)
+    base_price = fields.Float(string='Base Price',default=0.00)
     
-    additional_fee = fields.Float(string='Additional Fee', default=0.00)
+    additional_fee = fields.Float(string='Additional Fee', default=10.00)
     
     total_price = fields.Float(string='Total Price', readonly=True)
     
-    @api.onchange('base_price', 'additional_fee')
+    @api.onchange('base_price','additional_fee')
     def _onchange_total_price(self):
-        if self.base_price<0.00:
+        if self.base_price < 0.00:
             raise UserError('Base Price cannot be set as Negative.')
-        
-        self.total_price = self.base_price + self.additional_fee
-        
+     
+            self.total_price = self.base_price + self.additional_fee
+    
     @api.constrains('additional_fee')
     def _check_additional_fee(self):
         for record in self:
-            if record.additional_fee < 10.00:
-                raise ValidationError('Additional Fees cannot be less than 10.00: %s' % record.additional_fee)
+            if record.additional_fee<10.00:
+                raise ValidationError('Additional Fee cannot be less than 10.00: %s' % record.additional_fee)
+    
+    
+    
