7927728ab430b5337e8de2573a6f8321a92c33b9
WILLIAM <williamlkengne@gmail.com>
2021-09-21 14:15:08 +0000

    [ADD] ORMGOOD MODULE

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -15,3 +16,22 @@ class Course(models.Model):
                                        ('advanced', 'Advanced')],
                             copy=False)
     active = fields.Boolean(string='Active',default=True)
+    
+    base_price = fields.Float(string='Active', default=True)
+    
+    additional_fee = fields.Float(string='Additional Fee', default=0.00)
+    
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price<0.00:
+            raise UserError('Base Price cannot be set as Negative.')
+        
+        self.total_price = self.base_price + self.additional_fee
+        
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional Fees cannot be less than 10.00: %s' % record.additional_fee)
