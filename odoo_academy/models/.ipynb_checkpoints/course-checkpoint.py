540a99bd8918b79f9e18c892f55cb6430e3fff19
WILLIAM <williamlkengne@gmail.com>
2021-09-25 13:59:16 +0000

    [ADD] INHERITED3 MODULE

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -7,7 +7,7 @@ class Course(models.Model):
     
     _name = 'academy.course'
     _description = 'Course Info'
-#    
+   
     name = fields.Char(string = 'Title', required = True)
     description = fields.Text(string='Description')
     
