7997902786f8734988d23ad3dd4687b262c77d23
WILLIAM <williamlkengne@gmail.com>
2021-09-25 14:04:56 +0000

    [ADD] INHERITED4 MODULE

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
-    
+#    
     _name = 'academy.course'
     _description = 'Course Info'
    
