c27de17b750621538fa5aaaae57c38609d902923
WILLIAM <williamlkengne@gmail.com>
2021-09-27 16:26:12 +0000

    [ADD] INHERITED24 MODULE

--- odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -16,7 +16,7 @@ class Session(models.Model):
     
     instructor_id= fields.Many2one(comodel_name='res.partner', string='Instructor')
     
-    student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+    student_id = fields.Many2many(comodel_name='res.partner', string='Students')
     
     start_date = fields.Date(string='Start Date',
                              default=fields.Date.today)
