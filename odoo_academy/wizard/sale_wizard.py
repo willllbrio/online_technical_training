984f6eb30e5652d5f4c522baa37bcf67aec80f5f
WILLIAM <williamlkengne@gmail.com>
2021-09-28 22:21:42 +0000

    [ADD] INHERITED10 MODULE

--- odoo_academy/wizard/sale_wizard.py
+++ odoo_academy/wizard/sale_wizard.py
@@ -25,7 +25,7 @@ class SaleWizard(models.TransientModel):
 
     def create_sale_orders(self):
 
-        session_product_id = self.env['product_product'].search([('is_session_product','=',True)], limit=1)
+        session_product_id = self.env['product.product'].search([('is_session_product','=',True)], limit=1)
         if session_product_id:
             for student in self.student_ids:
                 order_id = self.env['sale.order'].create({
