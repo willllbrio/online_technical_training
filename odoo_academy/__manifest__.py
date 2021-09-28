9df9cb0e8434a82d6dc4232f5fd5e7526042d5d8
WILLIAM <williamlkengne@gmail.com>
2021-09-28 19:18:57 +0000

    [ADD] WIZARD TRANSIENT MODEL MODULE

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -28,6 +28,8 @@
         'views/course_views.xml',  
         'views/session_views.xml',
         'views/sale_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
        
     ],
     
--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -28,6 +28,15 @@ class Session(models.Model):
                            compute='_compute_end_date',
                            inverse='_inverse_end_date',
                            store=True)
+    state = fields.Selection(string='States',
+                             selection=[('draft','Draft'),
+                                        ('open', 'In Progress'),
+                                        ('canceled','Canceled')],
+                             default='draft',
+                             required=True)
+
+    total_price = fields.Float(string='Total Price',
+                               related='course_id.total_price')
     
     @api.depends('start_date','duration')
     def _compute_end_date(self):
