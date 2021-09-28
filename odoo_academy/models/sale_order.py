8b48193a160308b1c81cbcbb1a0555819017d86d
WILLIAM <williamlkengne@gmail.com>
2021-09-28 07:39:46 +0000

    [ADD] AV MODULE

--- odoo_academy/models/sale_order.py
+++ odoo_academy/models/sale_order.py
@@ -7,7 +7,7 @@
 #	session_id = fields.Many2one(comodel_name='academy.session',
 #								 string='Related Session',
 #								 ondelete='set null')
-#
+
 #	instructor_id = fields.Many2one(string='Session Instructor',
 #		 							related='session_id.instructor_id')
 #
