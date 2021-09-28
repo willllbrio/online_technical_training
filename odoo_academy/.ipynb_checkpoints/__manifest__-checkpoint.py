beba6d66a0ee77d31bb95b73907408097baf2a16
WILLIAM <williamlkengne@gmail.com>
2021-09-28 23:46:48 +0000

    [ADD] QWEB MODULE

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -27,7 +27,10 @@
         'views/academy_menuitems.xml',
         'views/course_views.xml',  
         'views/session_views.xml',
-#        'views/sale_views_inherit.xml',
+        'views/sale_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
+        'report/session_report_templates.xml',
        
     ],
     
