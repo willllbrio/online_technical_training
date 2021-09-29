961ae1464b8a4d0cd2ff813c965d62b41ec2af19
WILLIAM <williamlkengne@gmail.com>
2021-09-29 14:06:04 +0000

    [ADD] TRANSLATION MODULE

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
@@ -31,7 +31,7 @@ class Course(models.Model):
     @api.onchange('base_price','additional_fee')
     def _onchange_total_price(self):
         if self.base_price < 0.00:
-            raise UserError('Base Price cannot be set as Negative.')
+            raise UserError(_('Base Price cannot be set as Negative.'))
             
         self.total_price = self.base_price + self.additional_fee
     
@@ -39,7 +39,7 @@ class Course(models.Model):
     def _check_additional_fee(self):
         for record in self:
             if record.additional_fee<10.00:
-                raise ValidationError('Additional Fee cannot be less than 10.00: %s' % record.additional_fee)
+                raise ValidationError(_('Additional Fee cannot be less than 10.00: %s' % record.additional_fee))
     
     
     
